87d2875f6b391686f7ecfc45d7e88e780ae783bf
SEUNGHAN BAEK <skud11311@gmail.com>
2024-03-01 17:43:45 +0900

    1231232132131321

--- src/test/kotlin/com/challengeteamkotlin/campdaddy/CampDaddyApplicationTests.kt
+++ src/test/kotlin/com/challengeteamkotlin/campdaddy/CampDaddyApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class CampDaddyApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+//	@Test
+//	fun contextLoads() {
+//	}
 
 }
